24f2eb9240a7da0b143d3d1bf746b7c0a36b4033
eunha <dmsgk2323@naver.com>
2022-12-01 19:26:05 +0900

    feat: 데이터 삭제하기

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,13 +1,13 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ diaryList }) => {
+const DiaryList = ({ onDelete, diaryList }) => {
   console.log(diaryList);
   return (
     <div className="">
       <h2>일기 리스트</h2>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} />
+          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
         ))}
       </div>
     </div>
