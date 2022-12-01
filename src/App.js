378188f36959822bbc238b2af97d136c39694fc8
eunha <dmsgk2323@naver.com>
2022-12-01 19:15:05 +0900

    데이터 추가하기 기능

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-
-const dummyList = [
-  {
-    id: 1,
-    author: "김은하",
-    content: "아 할거많다",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "김마요",
-    content: "아 할거 짱많다",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "아무개",
-    content: "아 할거 없다",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-];
+import { useRef, useState } from "react";
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "김은하",
+//     content: "아 할거많다",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "김마요",
+//     content: "아 할거 짱많다",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "아무개",
+//     content: "아 할거 없다",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+  //함수로 이벤트를 끌어올리는게 헷갈림.
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,79 +1,97 @@
 import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
+  //이렇게 돔 이벤트에 접근하면 어떤 돔 객체인지 어떻게 알 수 있는거지?
+  const authorInput = useRef();
+  const contentInput = useRef();
 
-    const authorInput = useRef();
-    const contentInput = useRef();
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+  //const [author, setAuthor] = useState("");
+  //const [content , setContent] = useState("");
 
-    const [state, setState ] = useState({
-        author : "" ,
-        content: "",
-        emotion: 1,
-})
-    //const [author, setAuthor] = useState("");
-    //const [content , setContent] = useState("");
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-
-    const handleChangeState = (e) => {
-        setState({
-            ...state,
-            [e.target.name] : e.target.value,
-        })
+  const handleSubmit = () => {
+    if (state.author.length < 1) {
+      //alert("작성자는 최소 1글자 이상 입력해주세요");
+      authorInput.current.focus();
+      return;
     }
-
-    const handleSubmit =  ()=> {
-        if (state.author.length < 1) {
-            //alert("작성자는 최소 1글자 이상 입력해주세요");
-            authorInput.current.focus();
-            return;
-        }
-        if ( state.content.length < 5 ) {
-            //alert("일기 본문은 최소 5글자 이상 입력해주세요");
-            contentInput.current.focus();
-            return;
-        }
-
-        alert("저장성공");
+    if (state.content.length < 5) {
+      //alert("일기 본문은 최소 5글자 이상 입력해주세요");
+      contentInput.current.focus();
+      return;
     }
+    //onCreate 함수 호출
+    onCreate(state.author, state.content, state.emotion);
+    alert("저장성공");
+    setState({ author: "", content: "", emotion: 1 });
+  };
 
-    return <div className="DiaryEditor">
-        <h2>오늘의 일기</h2>
-        <div>
-            <input ref={authorInput} name="author" value={state.author} onChange={handleChangeState
-                //setState(
-                   // {author:e.target.value,
-                   // content:state.content,
-                    // ...state 로 스프레드 하면 10개씩 객체가 늘어나는 경우에 쓸 수 있다. 
-                    //대신 뒤에 스프레드를 뒤에 써야함 
-                    //}
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          ref={authorInput}
+          name="author"
+          value={state.author}
+          onChange={
+            handleChangeState
+            //setState(
+            // {author:e.target.value,
+            // content:state.content,
+            // ...state 로 스프레드 하면 10개씩 객체가 늘어나는 경우에 쓸 수 있다.
+            //대신 뒤에 스프레드를 뒤에 써야함
+            //}
 
-                //) 
-            }/>
-        </div>
-        <div>
-            <textarea ref={contentInput} name="content" value={state.content} onChange={handleChangeState
-                /* setState({
+            //)
+          }
+        />
+      </div>
+      <div>
+        <textarea
+          ref={contentInput}
+          name="content"
+          value={state.content}
+          onChange={
+            handleChangeState
+            /* setState({
                     ...state,
                     content:e.target.value,
                 }
             
                )  */
-            }/>
-        </div>
-        <div>
-            <select name="emotion" value={state.emotion} onChange={handleChangeState}>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-                <option value={5}>5</option>
-
-            </select>
-        </div>
-        <div>
-            <button onClick={handleSubmit}>일기 저장하기</button>
-        </div>
-    </div>;
-}
+          }
+        />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
 
 export default DiaryEditor;
